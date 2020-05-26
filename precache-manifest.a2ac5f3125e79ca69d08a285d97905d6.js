24035348b025b2ea18ed91074ea33d3c5243fc28
Shai Demri <shaidemri@gmail.com>
2020-05-26 17:52:17 +0300

    Updates

---
+++ precache-manifest.a2ac5f3125e79ca69d08a285d97905d6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7f9a74e76e2bd68484bb7205a9c48b",
+    "url": "/Interactive-video/index.html"
+  },
+  {
+    "revision": "5199207d77997354bfcf",
+    "url": "/Interactive-video/static/js/10.aca46792.chunk.js"
+  },
+  {
+    "revision": "f744c5ba9ffe601d2792",
+    "url": "/Interactive-video/static/js/11.1480e84a.chunk.js"
+  },
+  {
+    "revision": "d542b29e4344b54519d8",
+    "url": "/Interactive-video/static/js/12.b6ca7e23.chunk.js"
+  },
+  {
+    "revision": "83cbd1fd05a133bb8bf1",
+    "url": "/Interactive-video/static/js/13.9424f2d3.chunk.js"
+  },
+  {
+    "revision": "4bb12ea4a78531c356b4",
+    "url": "/Interactive-video/static/js/14.7719dedf.chunk.js"
+  },
+  {
+    "revision": "3b80d5736c9971389ef8",
+    "url": "/Interactive-video/static/js/2.da5ae2c2.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/Interactive-video/static/js/2.da5ae2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea34d49a0804fc60c8c",
+    "url": "/Interactive-video/static/js/3.53efdf09.chunk.js"
+  },
+  {
+    "revision": "290339b7c59ec90ab8ee",
+    "url": "/Interactive-video/static/js/4.5107aade.chunk.js"
+  },
+  {
+    "revision": "8e0fde0032c2ce5110c9",
+    "url": "/Interactive-video/static/js/5.c6dd6536.chunk.js"
+  },
+  {
+    "revision": "ff8f0989b56de734dd06",
+    "url": "/Interactive-video/static/js/6.1bdecd6d.chunk.js"
+  },
+  {
+    "revision": "aea4e8e8b908ab30c47b",
+    "url": "/Interactive-video/static/js/7.dd03388d.chunk.js"
+  },
+  {
+    "revision": "e9dfa8c6f636cbe4e4c8",
+    "url": "/Interactive-video/static/js/8.0b253f8e.chunk.js"
+  },
+  {
+    "revision": "18b8604a2db79e93dd31",
+    "url": "/Interactive-video/static/js/9.b6ff5588.chunk.js"
+  },
+  {
+    "revision": "be8196954154fa91ff88",
+    "url": "/Interactive-video/static/js/main.6d50abf3.chunk.js"
+  },
+  {
+    "revision": "6abdbf4184208d6e515a",
+    "url": "/Interactive-video/static/js/runtime-main.94f3d7a5.js"
+  },
+  {
+    "revision": "f91824c53eda0c8cede8ce8156910fc4",
+    "url": "/Interactive-video/static/media/billionaire_v2.f91824c5.mp4"
+  }
+]);
