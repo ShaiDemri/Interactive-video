d7c0f2d23fa84e802eabe3765106693309406b53
Shai Demri <shaidemri@gmail.com>
2020-05-27 18:23:54 +0300

    minor code refactoring

--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -21,33 +21,32 @@ const useStyles = makeStyles((theme) => ({
     height: "100%",
   },
 }));
-const startTrackingPixel = "http://www.mocky.io/v2/5be098b232000072006496f5";
-const endTrackingPixel = "http://www.mocky.io/v2/5be098d03200004d006496f6";
+const START_TRACKING_PIXEL_URL =
+  "http://www.mocky.io/v2/5be098b232000072006496f5";
+const END_TRACKING_PIXEL_URL =
+  "http://www.mocky.io/v2/5be098d03200004d006496f6";
 
 const androidURI =
   "https://play.google.com/store/apps/details?id=com.huuuge.casino.texas&hl=en";
 const iosURI =
   "https://apps.apple.com/us/app/billionaire-casino-slots-777/id1098617974";
+
+const VIDEO_SCHEDULE = {
+  spin: 4,
+  download: 21,
+};
+const USER_INACTIVE_TIME = 10000;
+
 export default function VideoPlayer() {
   const classes = useStyles();
   const [isPlaying, setIsPlaying] = React.useState(true);
-  const play = () => {
-    setIsPlaying(true);
-  };
-  const pause = () => {
-    setIsPlaying(false);
-  };
-  const fireTrackingPixel = (url) => {
-    fetch(url);
-  };
-
   const [timeElapsed, setTimeElapsed] = React.useState(0);
   const [showSpinBtn, setShowSpinBtn] = React.useState(false);
   const [showDownloadBtn, setShowDownloadBtn] = React.useState(false);
   const [enterSpinCondition, setEnterSpinCondition] = React.useState(true);
   const vidRef = useRef();
+
   const handleInactivity = async () => {
-    await vidRef.current.seekTo(20.5, "seconds");
     await vidRef.current.seekTo(21, "seconds");
     setShowSpinBtn(false);
     setEnterSpinCondition(false);
@@ -57,17 +56,43 @@ export default function VideoPlayer() {
   let timer = React.useRef();
   const handleTimeJump = React.useCallback(() => {
     clearTimeout(timer.current);
-    timer.current = setTimeout(handleInactivityCb, 10000);
+    timer.current = setTimeout(handleInactivityCb, USER_INACTIVE_TIME);
   }, [handleInactivityCb]);
 
   React.useEffect(() => {
-    if (enterSpinCondition && Math.floor(timeElapsed) === 4) {
+    if (showSpinBtn) {
+      handleTimeJump();
+    }
+  }, [showSpinBtn, handleTimeJump]);
+  React.useEffect(() => {
+    if (enterSpinCondition && Math.floor(timeElapsed) === VIDEO_SCHEDULE.spin) {
       setEnterSpinCondition(false);
       pause();
       setShowSpinBtn(true);
     }
-    setShowDownloadBtn(timeElapsed >= 21);
+    setShowDownloadBtn(timeElapsed >= VIDEO_SCHEDULE.download);
   }, [timeElapsed, isPlaying, showSpinBtn, enterSpinCondition]);
+  const play = () => {
+    setIsPlaying(true);
+  };
+  const pause = () => {
+    setIsPlaying(false);
+  };
+  const fireTrackingPixel = (url) => {
+    fetch(url);
+  };
+  const onSpinClick = () => {
+    clearTimeout(timer.current);
+    play();
+    setShowSpinBtn(false);
+  };
+  const onDownloadClick = () => {
+    if (isIOS) {
+      window.open(iosURI);
+    } else {
+      window.open(androidURI);
+    }
+  };
   return (
     <div className={classes.root}>
       <ReactPlayer
@@ -76,15 +101,16 @@ export default function VideoPlayer() {
         width="100%"
         height="100%"
         controls={true}
+        playsinline={true}
         ref={vidRef}
         volume={0.01}
         playing={isPlaying}
         onStart={() => {
           play();
-          fireTrackingPixel(startTrackingPixel);
+          fireTrackingPixel(START_TRACKING_PIXEL_URL);
         }}
         onEnded={() => {
-          fireTrackingPixel(endTrackingPixel);
+          fireTrackingPixel(END_TRACKING_PIXEL_URL);
         }}
         style={{ position: "relative" }}
         progressInterval={300}
@@ -105,14 +131,8 @@ export default function VideoPlayer() {
           text="spin"
           color={"blue"}
           icon={LoopIcon}
-          onclick={() => {
-            clearTimeout(timer.current);
-            play();
-            setShowSpinBtn(false);
-          }}
-        >
-          {handleTimeJump()}
-        </OverlayButton>
+          onclick={onSpinClick}
+        ></OverlayButton>
       ) : (
         <></>
       )}
@@ -120,13 +140,7 @@ export default function VideoPlayer() {
         <OverlayButton
           text="Download now!!!"
           icon={GetAppIcon}
-          onclick={() => {
-            if (isIOS) {
-              window.open(iosURI);
-            } else {
-              window.open(androidURI);
-            }
-          }}
+          onclick={onDownloadClick}
         />
       ) : (
         <></>
